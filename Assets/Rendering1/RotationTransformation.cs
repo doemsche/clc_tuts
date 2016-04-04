ed6250b6b04f93c6aac733456eec9c0a0a3d341b
doemsche <doemsche@gmx.ch>
2016-04-04 10:36:45 +0200

    transform updates

---
+++ Assets/Rendering1/RotationTransformation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Renderering1{
+	public class RotationTransformation : Transformation {
+		public Vector3 rotation;
+
+		public override Vector3 Apply (Vector3 point) {
+			float radZ = rotation.z * Mathf.Deg2Rad;
+			float sinZ = Mathf.Sin(radZ);
+			float cosZ = Mathf.Cos(radZ);
+
+			return point;
+		}
+	}
+}
