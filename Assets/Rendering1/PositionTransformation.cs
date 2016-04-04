c2be3aa6f796583a5983af486414823eb41d1075
doemsche <doemsche@gmx.ch>
2016-04-04 09:56:57 +0200

    add clock, factal, rendering1

---
+++ Assets/Rendering1/PositionTransformation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Renderering1{
+	public class PositionTransformation : Transformation {
+		public Vector3 position;
+
+		public override Vector3 Apply (Vector3 point)
+		{
+			return point + position;
+		}
+	}
+}
---
+++ Assets/Rendering1/Transformation.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+namespace Renderering1{
+	public abstract class Transformation : MonoBehaviour {
+		public abstract Vector3 Apply(Vector3 point);
+	}
+}
+
